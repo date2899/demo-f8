fa0ace07577978576876659f49f5b8782529a287
date2899 <date2899@gmail.com>
2022-06-01 11:42:17 +0700

    update

--- src/Content.js
+++ src/Content.js
@@ -1,22 +1,49 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
+const lessons = [
+	{
+		id: 1,
+		name: "ReactJS là gì? Tại sao nên học ReactJS",
+	},
+	{
+		id: 2,
+		name: "SPA/MPA là gì?",
+	},
+	{
+		id: 3,
+		name: "Arrow function",
+	},
+];
+
 function Content() {
-	const [count, setCount] = useState(180);
+	const [lessonId, setLessonId] = useState(1);
 
 	useEffect(() => {
-		const timerId = setInterval(() => {
-			setCount((prevent) => prevent - 1);
-			console.log("count...");
-		}, 1000);
-
-		return () => clearInterval(timerId);
-	}, []);
+		const handleComment = (detail) => {
+			console.log(detail);
+		};
+		window.addEventListener(`lesson-${lessonId}`, handleComment);
+		return () => {
+			window.removeEventListener(`lesson-${lessonId}`, handleComment);
+		};
+	}, [lessonId]);
 
 	return (
 		<div>
-			{count}
-			{console.log(1)}
+			<ul>
+				{lessons.map((lesson) => (
+					<li
+						key={lesson.id}
+						style={{
+							color: lessonId === lesson.id ? "red" : "#333",
+						}}
+						onClick={() => setLessonId(lesson.id)}
+					>
+						{lesson.name}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function emitComment(id) {
+	setInterval(() => {
+		window.dispatchEvent(
+			new CustomEvent(`lesson-${id}`, {
+				detail: ` Noi dung comment cua lesson ${id}`,
+			})
+		);
+	}, 2000);
+}
+emitComment(1);
+emitComment(2);
+emitComment(3);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
